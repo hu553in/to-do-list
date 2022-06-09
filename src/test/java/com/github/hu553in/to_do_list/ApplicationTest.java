d414141eecb7c6091fd0935e33b880197aa5f54d
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2022-06-09 16:21:29 +0600

    refactor: minor changes

--- src/test/java/com/github/hu553in/to_do_list/ApplicationTest.java
+++ src/test/java/com/github/hu553in/to_do_list/ApplicationTest.java
@@ -1,18 +1,22 @@
 package com.github.hu553in.to_do_list;
 
 import com.github.hu553in.to_do_list.controller.TaskController;
-import com.github.hu553in.to_do_list.test_util.AbstractSpringBootIntegrationTest;
+import com.github.hu553in.to_do_list.test_util.AbstractDatabaseContainerTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-class ApplicationTest extends AbstractSpringBootIntegrationTest {
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationTest extends AbstractDatabaseContainerTest {
 
     @Autowired
     private TaskController taskController;
 
     @Test
-    void testLoadApplicationContext() {
+    void givenSpringBootApp_whenRunTest_thenLoadAppContext() {
         Assertions.assertThat(taskController).isNotNull();
     }
 
--- src/test/java/com/github/hu553in/to_do_list/test_util/AbstractDatabaseContainerTest.java
+++ src/test/java/com/github/hu553in/to_do_list/test_util/AbstractDatabaseContainerTest.java
@@ -1,7 +1,5 @@
 package com.github.hu553in.to_do_list.test_util;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
@@ -9,16 +7,13 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
 @Testcontainers
-@ActiveProfiles("test")
-public abstract class AbstractSpringBootIntegrationTest {
+public abstract class AbstractDatabaseContainerTest {
 
-    private static final String POSTGRES_DOCKER_IMAGE_NAME = "postgres:14-alpine";
+    private static final String IMAGE_NAME = "postgres:14-alpine";
 
     @Container
-    private static final JdbcDatabaseContainer<?> POSTGRES_CONTAINER =
-            new PostgreSQLContainer<>(POSTGRES_DOCKER_IMAGE_NAME);
+    private static final JdbcDatabaseContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>(IMAGE_NAME);
 
     @DynamicPropertySource
     static void setDynamicProperties(final DynamicPropertyRegistry registry) {
